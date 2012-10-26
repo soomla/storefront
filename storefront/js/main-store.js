eecad9800c6904e742af7ba47b892f93981962d1
Gur Dotan <gurdotan@gmail.com>
2012-10-26 17:12:30 +0200

    Use Backbone dev version

--- storefront/js/main-store.js
+++ storefront/js/main-store.js
@@ -13,7 +13,7 @@ require.config({
         modernizr           	: "libs/modernizr-2.5.3.min",   // From Modernizr docs: If you don't support IE8 and don't need to worry about FOUC, feel free to include modernizr.js whereever
         less                	: "libs/less-1.3.0.min",
         underscore          	: "libs/underscore-min",
-        backbone            	: "libs/backbone/backbone-min",
+        backbone            	: "libs/backbone/backbone",
         backboneRelational  	: "libs/backbone/backbone-relational",
         marionette          	: "libs/backbone/backbone.marionette",
         handlebars          	: "libs/handlebars-1.0.0.beta.6"
