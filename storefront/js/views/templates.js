52f8c0899fbe4c8eb48c4bb6af04c90f4707a1d0
Gur Dotan <gurdotan@gmail.com>
2013-01-06 17:34:47 +0200

    Add a font stylesheet Handlebars helper

--- storefront/js/views/templates.js
+++ storefront/js/views/templates.js
@@ -4,6 +4,34 @@ define(["handlebars", "underscore", "jquery"], function(Handlebars, _, $) {
         return parseFloat(value).toFixed(2);
     });
 
+    // A helper for generating @font-face CSS directives from the
+    // theme's fonts, and classes using them
+    Handlebars.registerHelper("fontFaceStylesheet", function(fonts) {
+
+        var buffer = "<style>\n", i;
+        _.each(fonts, function(font, index) {
+            i = index + 1;
+
+            // Define the font face with a numbered name and source URL
+            buffer += "\t@font-face {\n";
+            buffer += "\t\tfont-family: FontFace" + i + ";\n";
+            buffer += "\t\tsrc: url('" + font.url + "');\n";
+
+            // Add additional font face properties if they exist
+            if (font.stretch)   buffer += "\t\tfont-stretch: "  + font.stretch  + ";\n";
+            if (font.weight)    buffer += "\t\tfont-weight: "   + font.weight   + ";\n";
+
+            // Add a numbered class that will apply the font face
+            buffer += "\t}\n";
+            buffer += "\t.font-face-" + i + "{ font-family: FontFace" + i + "; }\n";
+        });
+
+        buffer += "</style>";
+
+        // return the finished buffer
+        return new Handlebars.SafeString(buffer);
+    });
+
     _.extend(Handlebars, {
         _templatePath : "",
         setTemplatePath : function(templatePath) {
