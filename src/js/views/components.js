3bb7eafbb6ea9c7eb7bee42731bf4abc70c9e695
Gur Dotan <gurdotan@gmail.com>
2013-04-10 18:07:07 +0300

    CarouselView: assume the children are in 'this.children' container

--- src/js/views/components.js
+++ src/js/views/components.js
@@ -210,6 +210,7 @@ define(["jquery", "backbone", "viewMixins", "marionette", "cssUtils", "jquery.fa
 
     // TODO: Write unit test for this component
     var CarouselView = Marionette.CompositeView.extend({
+        tagName : "ul",
         initialize : function() {
             _.bindAll(this, "switchActive");
         },
@@ -219,25 +220,27 @@ define(["jquery", "backbone", "viewMixins", "marionette", "cssUtils", "jquery.fa
         },
         showNext : function() {
             this.activeIndex += 1;
-            if (this.activeIndex == this.keys.length) this.activeIndex = 0;
+            if (this.activeIndex === this.children.length) this.activeIndex = 0;
             this.switchActive().trigger("next");
         },
         showPrevious : function() {
             this.activeIndex -= 1;
-            if (this.activeIndex == -1) this.activeIndex = this.keys.length - 1;
+            if (this.activeIndex === -1) this.activeIndex = this.children.length - 1;
             this.switchActive().trigger("previous");
         },
         switchActive : function() {
             this.activeChild.$el.hide();
-            this.activeChild = this.children[this.keys[this.activeIndex]];
+            this.activeChild = this.getActiveChild();
             this.activeChild.$el.show();
             return this;
         },
+        getActiveChild : function() {
+            return this.children.findByIndex(this.activeIndex);
+        },
         onRender : function() {
             // Initialize variables necessary for next / previous functionality
-            this.keys        = _.keys(this.children);
             this.activeIndex = 0;
-            this.activeChild = this.children[this.keys[this.activeIndex]];
+            this.activeChild = this.getActiveChild();
 
             _.each(this.children, function(view) {
                 view.$el.hide();
