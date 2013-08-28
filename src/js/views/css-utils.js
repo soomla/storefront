49e81f58dd2687703402b14407d8a1e0402bb05c
Gur Dotan <gurdotan@gmail.com>
2013-08-28 13:23:10 +0300

    Add vendor prefixed detection of animationend event

--- src/js/views/css-utils.js
+++ src/js/views/css-utils.js
@@ -8,9 +8,39 @@ define("cssUtils", ["modernizr"], function() {
         'transition'       : 'transitionend'
     };
 
+
+    // Prepare variables for vendor prefixed animation event detection
+    var el              = document.createElement("p"),
+        animation       = false,
+        animationString = 'animation',
+        keyframePrefix  = '',
+        domPrefixes     = 'webkit Webkit moz Moz o ms MS khtml'.split(' '),
+        prefix          = '',
+        endSuffix;
+
+
+    // Detect animationend event name
+    if (el.style.animationName) animation = true;
+    if (animation === false) {
+        for (var i = 0; i < domPrefixes.length; i++) {
+            if (el.style[ domPrefixes[i] + 'AnimationName' ] !== undefined) {
+                prefix = domPrefixes[ i ];
+                animationString = prefix + 'Animation';
+                keyframePrefix = '-' + prefix.toLowerCase() + '-';
+                animation = true;
+                break;
+            }
+        }
+    }
+    endSuffix = (animationString.match(/^o?animation/)) ? "end" : "End";
+
+
     return {
         getTransitionendEvent : function() {
             return transEndEventNames[ Modernizr.prefixed('transition') ];
+        },
+        getAnimationendEvent : function() {
+            return animationString + endSuffix;
         }
     };
 });
