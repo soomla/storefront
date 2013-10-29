999dff182fa0d376993262e601fb2b16457c56a4
Gur Dotan <gurdotan@gmail.com>
2013-10-29 23:46:47 +0200

    Upgrade to Backbone Relational 0.8.6

--- src/js/main.js
+++ src/js/main.js
@@ -59,7 +59,7 @@ require.config({
         // Backbone related
         underscore          	: "libs/underscore-1.5.2",
         backboneFramework       : "libs/backbone/backbone-1.0.0",
-        backboneRelational  	: "libs/backbone/backbone-relational-0.8.5",
+        backboneRelational  	: "libs/backbone/backbone-relational-0.8.6",
         backboneExtensions      : "libs/backbone/backbone-extensions",
         marionetteFramework 	: "libs/backbone/backbone.marionette.core-1.0.3",
         marionetteExtensions    : "libs/backbone/marionette-extensions",
