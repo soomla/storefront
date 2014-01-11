c55b730eb258b24c1244632ff7c37065ce33beea
Gur Dotan <gurdotan@gmail.com>
2014-01-11 14:30:03 +0200

    Improve good packs:
    - Add a collection for all single use goods and maintain it
    - Add custom errors
    - Remove unused functions

--- src/js/models/models.js
+++ src/js/models/models.js
@@ -1,4 +1,4 @@
-define("models", ["backbone", "economyModels", "utils", "urls", "template", "assetManager", "hooks"], function(Backbone, EconomyModels, Utils, Urls, Template, Assets, Hooks) {
+define("models", ["backbone", "economyModels", "utils", "urls", "template", "assetManager", "hooks", "errors"], function(Backbone, EconomyModels, Utils, Urls, Template, Assets, Hooks, Errors) {
 
     // Cache base classes.
     var RelationalModel             = Backbone.RelationalModel;
@@ -36,6 +36,11 @@ define("models", ["backbone", "economyModels", "utils", "urls", "template", "ass
         var goodsMap    = this.goodsMap     = {};
         var packsMap    = this.packsMap     = {};
         var categoryMap = this.categoryMap  = {};
+
+        // Hold all single use goods in a collection for quick retrieval
+        // and event listening
+        this.singleUseGoods = new Backbone.Collection([], {comparator : "name"});
+        this.singleUseGoods.listenTo(this.singleUseGoods, "change:name", this.singleUseGoods.sort);
 
         // Initialize the economy only with currencies, since their raw
         // representation fits what's expected.  In contrast, raw categories need
@@ -85,6 +90,7 @@ define("models", ["backbone", "economyModels", "utils", "urls", "template", "ass
 
                             // By default instantiate goods like this
                             good = new SingleUseGood(rawGood);
+                            this.singleUseGoods.add(good);
                             break;
                     }
                 }
@@ -316,6 +322,9 @@ define("models", ["backbone", "economyModels", "utils", "urls", "template", "ass
                 good,
                 category;
 
+            // Adding single use packs requires that at least one single use good exists
+            if (type === "goodPacks" && this.getSingleUseGoods().isEmpty()) throw new Errors.NoSingleUseGoodsError();
+
             if (this.supportsMarketPurchaseTypeOnly()) {
 
                 // For market purchase only stores, no need to consider all good types, categories or currencies
@@ -375,6 +384,9 @@ define("models", ["backbone", "economyModels", "utils", "urls", "template", "ass
                 });
                 good.setCurrencyId(firstCurrencyId);
 
+                // For good packs, assign an arbitrary good item ID
+                if (good.is("goodPacks")) good.setGoodItemId(this.getSingleUseGoods().first().id);
+
                 // Ensure the model has an asset assigned
                 // before adding it to the collection (which triggers a render)
                 if (type === "upgradable") {
@@ -404,6 +416,9 @@ define("models", ["backbone", "economyModels", "utils", "urls", "template", "ass
 
             // Add good to category
             category.getGoods().add(good, {at: 0});
+
+            // Add the good to the single use collection if applicable
+            if (type === "singleUse") this.singleUseGoods.add(good);
             return good;
         },
         addUpgrade : function(options) {
@@ -500,13 +515,15 @@ define("models", ["backbone", "economyModels", "utils", "urls", "template", "ass
 
                 // Remove zero-index bar
                 this.assets.removeItemAsset(good.getEmptyUpgradeBarAssetId());
-            }
 
-            if (good.is("singleUse")) {
+            } else if (good.is("singleUse")) {
                 var goodPacks = this.getGoodPacksForSingleUseGood(good);
 
                 // Remove all good packs associated with this single use good
                 this._clearReverseOrder(goodPacks, this.removeVirtualGood);
+
+                // Remove from single use goods collection
+                this.singleUseGoods.remove(good);
             }
 
             // Remove from mappings
@@ -640,25 +657,11 @@ define("models", ["backbone", "economyModels", "utils", "urls", "template", "ass
         },
         getGoodPacksForSingleUseGood: function (singleUseGood) {
             return _.filter(this.goodsMap, function(good) {
-                return good.is("goodsPack") && good.getGoodItemId() === singleUseGood.id;
+                return good.is("goodPacks") && good.getGoodItemId() === singleUseGood.id;
             });
         },
         getSingleUseGoods : function() {
-            return this._getGoodsByType("singleUse");
-        },
-        getLifetimeGoods : function() {
-            return this._getGoodsByType("lifetime");
-        },
-        getEquippableGoods : function() {
-            return this._getGoodsByType("equippable");
-        },
-        getUpgradableGoods : function() {
-            return this._getGoodsByType("upgradable");
-        },
-        _getGoodsByType : function(type) {
-            return _.filter(this.goodsMap, function(good) {
-                return good.is(type);
-            });
+            return this.singleUseGoods;
         },
         toJSON : function() {
 
