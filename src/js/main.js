5808f8b822fdd64a3ea90edc73a43f112e80949a
Gur Dotan <gurdotan@gmail.com>
2014-03-13 23:28:11 +0200

    Move view related libraries to storefront

--- src/js/main.js
+++ src/js/main.js
@@ -16,19 +16,10 @@ require.config({
         models                  : "models/models",
         hooks                   : "models/hooks",
 
-        // Views
-        components              : "views/components",
-        itemViews               : "views/item-views",
-        expandableItemViews     : "views/expandable-item-views",
-        collectionViews         : "views/collection-views",
-        helperViews             : "views/helper-views",
-        templates           	: "views/templates",
-
         // Utilities
         utils                   : "utils/utils",
         stringUtils             : "utils/string-utils",
         urls                    : "utils/urls",
-        userAgent               : "utils/user-agent",
         constants               : "utils/constants",
         errors                  : "utils/errors",
 
@@ -45,23 +36,35 @@ require.config({
         nativeApiStubs          : "storefront/native-api-stubs",
         jsAPI                   : "storefront/js-api",
         messaging               : "storefront/messaging",
-        cssUtils                : "storefront/utils/css-utils",
 
-        // 3rd party modules
-        imagesloaded            : "libs/imagesloaded-3.0.4",
-        eventie                 : "libs/eventie-1.0.3",
-        eventEmitter            : "libs/event-emitter-4.2.0",
-        fastbutton              : "libs/google.fastbutton",
-        less                	: "libs/less-1.3.3",
-        iscroll                 : "libs/iscroll",
-        handlebars          	: "libs/handlebars-1.0.rc.2",
+        // Storefront utilities
+        cssUtils                : "storefront/utils/css-utils",
+        userAgent               : "storefront/utils/user-agent",
+
+        // Storefront views
+        itemViews               : "storefront/views/item-views",
+        expandableItemViews     : "storefront/views/expandable-item-views",
+        collectionViews         : "storefront/views/collection-views",
+        helperViews             : "storefront/views/helper-views",
+        templates           	: "storefront/views/templates",
+        components              : "storefront/views/components",
+
+
+        // Storefront - 3rd party modules
+        imagesloaded            : "storefront/libs/imagesloaded-3.0.4",
+        eventie                 : "storefront/libs/eventie-1.0.3",
+        eventEmitter            : "storefront/libs/event-emitter-4.2.0",
+        less                	: "storefront/libs/less-1.3.3",
+        fastbutton              : "storefront/libs/google.fastbutton",
+        iscroll                 : "storefront/libs/iscroll",
+        handlebars          	: "storefront/libs/handlebars-1.0.rc.2",
 
         // jQuery related
         jquery              	: "libs/jquery/jquery-1.9.1.min",
-        "jquery.fastbutton"     : "libs/jquery/jquery.google.fastbutton",
-        "jquery.preload"        : "libs/jquery/jquery.preload",
-        "jquery.pnotify"        : "libs/jquery/jquery.pnotify",
-        jqueryUtils             : "libs/jquery/jquery-utils",
+        "jquery.pnotify"        : "storefront/libs/jquery/jquery.pnotify",
+        jqueryUtils             : "storefront/libs/jquery/jquery-utils",
+        "jquery.preload"        : "storefront/libs/jquery/jquery.preload",
+        "jquery.fastbutton"     : "storefront/libs/jquery/jquery.google.fastbutton",
 
         // Backbone related
         underscore          	: "libs/underscore-1.5.2",
@@ -84,12 +87,6 @@ require.config({
         "imagesloaded": {
             deps: ['jquery']
         },
-        "jquery.preload": {
-            deps: ['jquery']
-        },
-        "jquery.fastbutton": {
-            deps: ['jquery', 'fastbutton']
-        },
         "backboneFramework" : {
             deps: ['underscore'],
             exports: 'Backbone'
@@ -109,6 +106,15 @@ require.config({
         },
         templates : {
             deps: ['handlebars']
+        },
+
+
+        // Storefront related shims
+        "jquery.fastbutton": {
+            deps: ['jquery', 'fastbutton']
+        },
+        "jquery.preload": {
+            deps: ['jquery']
         }
 
         // No need to export globally in 'shim' section
